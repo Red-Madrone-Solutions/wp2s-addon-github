64019f026087979f804ad32c5d8707b7a0f39c71
Matt Vanderpol <matt@vanderpol.net>
2020-04-28 14:26:38 -0700

    Move encryption setup to EncryptedOption
    It makes more sense to have it here where it’s needed

--- src/EncryptedOption.php
+++ src/EncryptedOption.php
@@ -5,6 +5,30 @@ namespace RMS\WP2S\GitHub;
 if ( !defined('ABSPATH') ) exit;
 
 class EncryptedOption extends Option {
+    private static function encryption_key_file() {
+        $bits = wp_get_upload_dir();
+        return $bits['basedir'] . '/.rms-wp2s-gh-enc-key';
+    }
+
+    private static function hash_salt_file() {
+        $bits = wp_get_upload_dir();
+        return $bits['basedir'] . '/.rms-wp2s-gh-hash-salt';
+    }
+
+    public static function setup($overwrite = false) {
+        $key_file = self::encryption_key_file();
+        if ( !file_exists($key_file) || $overwrite ) {
+            $key = Util::random_bytes(32);
+            file_put_contents($outfile, base64_encode($key));
+        }
+
+        $salt_file = self::hash_salt_file();
+        if ( !file_exists($salt_file) || $overwrite ) {
+            $salt = Util::random_bytes(16);
+            file_put_contents($outfile, base64_encode($salt));
+        }
+    }
+
     public function type() {
         return 'password';
     }
