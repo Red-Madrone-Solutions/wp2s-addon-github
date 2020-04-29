c0ba227e94973a9927b664236f9734966b4d03c6
Matt Vanderpol <matt@vanderpol.net>
2020-04-29 14:37:04 -0700

    Get Repo and Account details from OptionSet

--- src/Client.php
+++ src/Client.php
@@ -6,6 +6,8 @@ if ( !defined('ABSPATH') ) exit;
 
 class Client {
     private $option_set;
+    private $account = null;
+    private $repo = null;
 
     public function __construct($option_set) {
         $this->option_set = $option_set;
@@ -20,5 +22,27 @@ class Client {
             return $token_option->value($decrypt = true);
         }
         throw new \Exception('Cannot find token');
+    }
+
+    private function account() : string {
+        if ( is_null($this->account) ) {
+            if ( $account_option = $this->option_set->findByName('account') ) {
+                $this->account =$account_option->value();
+            } else {
+                throw new \Exception('Cannot find account');
+            }
+        }
+        return $this->account;
+    }
+
+    private function repo() : string {
+        if ( is_null($this->repo) ) {
+            if ( $repo_option = $this->option_set->findByName('repository') ) {
+                $this->repo =$repo_option->value();
+            } else {
+                throw new \Exception('Cannot find repo');
+            }
+        }
+        return $this->repo;
     }
 }
