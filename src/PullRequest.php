0e8427a594aba6cfb5811fc2077d9bdc7c72e40e
Matt Vanderpol <matt@vanderpol.net>
2020-05-04 20:08:30 -0700

    Encapsulate PR details in an object

---
+++ src/PullRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace RMS\WP2S\GitHub;
+
+if ( !defined('ABSPATH') ) exit;
+
+class PullRequest {
+    protected $client;
+    private $pull_number;
+
+    public function __construct(int $pull_number, Client $client) {
+        $this->pull_number = $pull_number;
+        $this->client      = $client;
+    }
+
+    public function merge() {
+        return $this->client->merge_pull_request($this);
+    }
+
+    public function pull_number() {
+        return $this->pull_number;
+    }
+}
