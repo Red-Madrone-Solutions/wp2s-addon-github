e1edf37669af5bd2071a62c41006c0bf7c1f8fb7
Matt Vanderpol <matt@vanderpol.net>
2020-05-08 10:12:54 -0700

    Find the files that should be deleted
    These are files that exist in the deploy cache but not in the file list for the branch

--- src/DeployCache.php
+++ src/DeployCache.php
@@ -12,4 +12,16 @@ class DeployCache {
     public static function fileIsCached(string $local_path) {
         return \WP2Static\DeployCache::fileisCached($local_path, self::CACHE_NAMESPACE);
     }
+
+    public static function findDeleted(FileList $filesystem_files) {
+        $deleted_files = new FileList();
+        $deploy_cache_files = \WP2Static\DeployCache::getPaths();
+        foreach ( $deploy_cache_files as $cache_key ) {
+            if ( !$filesystem_files->cacheKeyExists($cache_key) ) {
+                $filepath = File::pathForCacheKey($cache_key);
+                $deleted_files->addFile(new File($filepath, $needs_delete = true));
+            }
+        }
+        return $deleted_files;
+    }
 }
