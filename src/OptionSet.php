e2969dc8d6dedc0fa61eac89b9673bca91ee9eb2
Matt Vanderpol <matt@vanderpol.net>
2020-05-11 10:51:55 -0700

    Adding/updating options for needs

--- src/OptionSet.php
+++ src/OptionSet.php
@@ -18,8 +18,12 @@ class OptionSet implements \IteratorAggregate {
     protected function setup() {
         $this->list[]= new Option('account', 'Account', 'The account at GitHub that owns the repository you want to deploy to');
         $this->list[]= new Option('repository', 'Repository');
-        $this->list[]= new Option('branch', 'Branch');
+        $this->list[]= new Option('source_branch', 'Source Branch', 'Branch in GitHub that a PR for the target branch should be based on');
+        $this->list[]= new Option('target_branch', 'Target Branch', 'Branch in GitHub that a PR for the deploy should be targeted at.');
         $this->list[]= new EncryptedOption('personal_access_token', 'Personal Access Token', 'Not displayed for security. Enter an invalid token value to disable.');
+        $this->list[]= new Option('commit_message', 'Commit Message', 'Message to use for commit to GitHub');
+        $this->list[]= new Option('pr_title', 'PR Title', 'Text to use for title of Pull Request');
+        $this->list[]= new Option('pr_body', 'PR Body', 'Text to use for body of Pull Request');
         // $this->list[]= new DisabledOption('subdirectory', 'Subdirectory');
         // $this->list[]= new DisabledOption('commit_message', 'Commit Message');
     }
