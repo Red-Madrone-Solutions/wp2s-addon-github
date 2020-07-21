95ad47708fe2e4870c0935519b347b5726f6a2f3
Matt Vanderpol <matt@vanderpol.net>
2020-07-21 10:37:46 -0700

    No need to pass and save a database reference

--- src/DatabaseFileMapper.php
+++ src/DatabaseFileMapper.php
@@ -5,13 +5,6 @@ namespace RMS\WP2S\GitHub;
 if ( !defined('ABSPATH') ) exit; // phpcs:ignore
 
 class DatabaseFileMapper extends FileMapper {
-    private $database = null;
-
-    public function __construct($database) {
-        $this->database = $database;
-        parent::__construct();
-    }
-
     protected function load_details(string $path_hash) {
         return Database::instance()->getFileDetails(
             $path_hash,
