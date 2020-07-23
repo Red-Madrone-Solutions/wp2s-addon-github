2a4b91b51cdbc74c3ada79de9e81952de5b38337
Matt Vanderpol <matt@vanderpol.net>
2020-07-23 11:21:22 -0700

    Use test deployer and confirm file count
    Trying to figure out different run results between laptop and desktop

--- tests/Feature/DeployTest.php
+++ tests/Feature/DeployTest.php
@@ -21,7 +21,8 @@ it('Deploys', function() {
     global $temp_dir;
     $_ENV['RMS_WP2S_GitHub_Encryption_Key'] = 'encryption key';
     $_ENV['RMS_WP2S_GitHub_Salt']           = 'salt';
-    $deployer = new Deployer();
+    $deployer = new TestDeployer();
     $deployer->setup($temp_dir, new TestFileMapper(), new TestOptionSet(), new TestClient());
     $deployer->execute();
+    assertEquals(3, $deployer->file_list()->count());
 })->group('integration');
