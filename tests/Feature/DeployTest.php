365afa7ec54a9e8207b97fd5c41fe7f892e6ba61
Matt Vanderpol <matt@vanderpol.net>
2020-07-24 13:57:36 -0700

    Use actual Client class but test Request for more complete testing

--- tests/Feature/DeployTest.php
+++ tests/Feature/DeployTest.php
@@ -21,8 +21,10 @@ it('Deploys', function() {
     global $deploy_test_temp_dir;
     $_ENV['RMS_WP2S_GitHub_Encryption_Key'] = 'encryption key';
     $_ENV['RMS_WP2S_GitHub_Salt']           = 'salt';
+    $option_set = new TestOptionSet();
+    $client = new \RMS\WP2S\GitHub\Client($option_set, '\Tests\TestRequest');
     $deployer = new TestDeployer();
-    $deployer->setup($deploy_test_temp_dir, new TestFileMapper(), new TestOptionSet(), new TestClient());
+    $deployer->setup($deploy_test_temp_dir, new TestFileMapper(), $option_set, $client);
     $deployer->execute();
     assertEquals(3, $deployer->file_list()->count());
 })->group('integration');
